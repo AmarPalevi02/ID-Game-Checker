d447e60157b054f16c2e690853590903ab7b2c20
amarNrddn <amarnrddn02@gmail.com>
2024-05-20 15:37:50 +0700

    feat: get api free-fire

--- src/config/index.js
+++ src/config/index.js
@@ -1,3 +1,5 @@
  export const configs = {
-   base_url: process.env.REACT_APP_BASE_URL
+   base_url: process.env.REACT_APP_BASE_URL,
+   api_key: process.env.REACT_API_KEY,
+   api_host: process.env.REACT_API_HOST 
 }
--- src/utils/fetch.js
+++ src/utils/fetch.js
@@ -44,3 +44,25 @@ export const getPUBG = async (id) => {
       }
    }
 }
+
+export const GetFreeFire = async (id) => {
+   try {
+      const URL = await axios.get(`${configs.base_url}/free-fire/${id}`, {
+         headers: {
+            'X-RapidAPI-Key': '21cad4bf71msh475c1b32967d6ebp19d0b6jsn32d87c30f531',
+            'X-RapidAPI-Host': 'id-game-checker.p.rapidapi.com'
+         }
+      })
+
+      if (URL.data && URL.data.error) {
+         throw new Error(URL.data.error)
+      }
+
+      return URL
+   } catch (error) {
+      return {
+         error: true,
+         msg: `Can't find a username for "User ID: ${id}".`
+      }
+   }
+}
